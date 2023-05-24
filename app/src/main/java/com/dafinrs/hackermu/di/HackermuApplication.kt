c51dc6f1443eaf887be4deaccbf8ae4871f607b8
Muhammad Dafi <dafinoer@outlook.com>
2023-05-24 23:33:37 +0700

    refactor code

--- app/src/main/java/com/dafinrs/hackermu/di/HackermuApplication.kt
+++ app/src/main/java/com/dafinrs/hackermu/di/HackermuApplication.kt
@@ -4,6 +4,4 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class HackermuApplication: Application() {
-
-}
+class HackermuApplication: Application()
